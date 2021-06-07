e39c11d08af53c9585f7d8b7871ae0bcd3520f46
Adudin <85031696+adudon01@users.noreply.github.com>
2021-06-07 10:53:57 +0700

    Update specialtools.py

--- userbot/modules/specialtools.py
+++ userbot/modules/specialtools.py
@@ -1,19 +1,18 @@
+# Credits By @VckyouuBitch From Geez-Project
+# Tolong Haragai ya:)
+# Kalo emg Bisa Menghargai seseorang pasti pahamm la ya:)
+# Credits © Geez - Projects
+
 import os
-from datetime import datetime as dt
-from random import choice
-from shutil import rmtree
 
 import moviepy.editor as m
-import pytz
-import requests
-from bs4 import BeautifulSoup as b
 
 from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.getaudio(?: |$)(.*)")
-async def daudtoid(event):
+@register(outgoing=True, pattern="^.getaudio(?: |$)(.*)", disable_errors=True)
+async def _(event):
     ureply = await event.get_reply_message()
     if not (ureply and ("audio" in ureply.document.mime_type)):
         await event.edit("`Reply To Audio Only..`")
@@ -25,8 +24,8 @@ async def daudtoid(event):
     await event.edit("`Done.. Now reply to video In which u want to add that Audio`")
 
 
-@register(outgoing=True, pattern="^.addaudio(?: |$)(.*)")
-async def adaudroid(event):
+@register(outgoing=True, pattern="^.addaudio(?: |$)(.*)", disable_errors=True)
+async def _(event):
     ureply = await event.get_reply_message()
     if not (ureply and ("video" in ureply.document.mime_type)):
         await event.edit("`Reply To Gif/Video In which u want to add audio.`")
@@ -53,7 +52,6 @@ async def adaudroid(event):
     os.remove(x)
     os.remove(ultt)
     await xx.delete()
-
 
 CMD_HELP.update(
     {
